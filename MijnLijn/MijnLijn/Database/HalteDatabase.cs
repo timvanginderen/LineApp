ce1cf26ba2d60d4716e6075e2793529c7ddbbf90
Tim Van Ginderen <timvanginderen@gmail.com>
2017-05-11 19:46:21 +0200

    Add halte model and database classes

---
+++ MijnLijn/MijnLijn/Database/HalteDatabase.cs
@@ -0,0 +1,45 @@
+﻿using MijnLijn.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MijnLijn.Database
+{
+    class HalteDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public HalteDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Halte>().Wait();
+        }
+
+        public Task<List<Halte>> GetItemsAsync()
+        {
+            return database.Table<Halte>().ToListAsync();
+        }
+
+        public Task<Halte> GetItemAsync(int id)
+        {
+            return database.Table<Halte>().Where(i => i.Z_PK == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(Halte item)
+        {
+            if (item.Z_PK != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(Halte item)
+        {
+            return database.DeleteAsync(item);
+        }
+    }
+}
---
+++ MijnLijn/MijnLijn/Models/Halte.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MijnLijn.Models
+{
+    class Halte
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Z_PK { get; set; }
+        public int ZEXTERNALID { get; set; }
+        public string ZNAME { get; set; }
+        public int NUMBER { get; set; }
+    }
+}

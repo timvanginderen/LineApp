5f2c17d3f73d52136288dc4e489fdfe48d395c2c
Tim Van Ginderen <timvanginderen@gmail.com>
2017-05-11 17:52:06 +0200

    Copy sqlite file to device specific location
    Copy file to Android app sandbox when invoked

--- MijnLijn/MijnLijn.Android/FileHelper.cs
+++ MijnLijn/MijnLijn.Android/FileHelper.cs
@@ -11,7 +11,30 @@ namespace MijnLijn.Droid
         public string GetLocalFilePath(string filename)
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            return Path.Combine(path, filename);
+            string dbPath = Path.Combine(path, filename);
+
+            CopyDatabaseIfNotExists(dbPath);
+
+            return dbPath;
+        }
+
+        private static void CopyDatabaseIfNotExists(string dbPath)
+        {
+            if (!File.Exists(dbPath))
+            {
+                using (var br = new BinaryReader(Android.App.Application.Context.Assets.Open("staticHaltes.sqlite")))
+                {
+                    using (var bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int length = 0;
+                        while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bw.Write(buffer, 0, length);
+                        }
+                    }
+                }
+            }
         }
     }
 }

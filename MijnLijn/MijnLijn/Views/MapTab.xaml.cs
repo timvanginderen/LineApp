1998660b360afa24bcbe7ed67a4f4cf11d6de63e
Tim Van Ginderen <timvanginderen@gmail.com>
2017-05-14 23:58:24 +0200

    Add geo locator plugin to all projects (WIP)

--- MijnLijn/MijnLijn/Views/MapTab.xaml.cs
+++ MijnLijn/MijnLijn/Views/MapTab.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using Plugin.Geolocator;
+using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +13,29 @@ namespace MijnLijn
         public MapTab()
         {
             InitializeComponent();
+
+            GetPosition();
+        }
+
+        public async void GetPosition()
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                var position = await locator.GetPositionAsync(10000);
+                if (position == null)
+                    return;
+
+                Debug.WriteLine("Position Status: {0}", position.Timestamp);
+                Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+                Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
+            }
         }
     }
 }

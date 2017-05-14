974cf0efc357fbd0dc2d64e15cca289c2db67a3c
Tim Van Ginderen <timvanginderen@gmail.com>
2017-05-14 15:51:59 +0200

    Add encryption util class, refactor rest service

--- MijnLijn/MijnLijn/Data/Remote/RestService.cs
+++ MijnLijn/MijnLijn/Data/Remote/RestService.cs
@@ -1,18 +1,20 @@
 ﻿using MijnLijn.Models;
+using MijnLijn.Utils;
 using Newtonsoft.Json;
-using PCLCrypto;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MijnLijn.Data.Remote
 {
     public class RestService : IRestService
     {
+        private const string FIELD_KEY_STOP_ID = "stopID";
+        private const string FIELD_KEY_OS = "os";
+        private const string FIELD_VALUE_OS = "android";
+
         HttpClient client;
 
         public List<Line> Lines { get; private set; }
@@ -26,11 +28,7 @@ namespace MijnLijn.Data.Remote
 
         public async Task<ApiResponse> PostToGetLines(int[] stopNumbers)
         {
-
-            client.DefaultRequestHeaders.Remove("x-token");
-
-            string token = CreateToken(stopNumbers);
-            client.DefaultRequestHeaders.Add("x-token", token);
+            AddRequestToken(stopNumbers);
 
             var uri = new Uri(string.Format(Constants.ApiUrl));
             ApiResponse apiResponse = new ApiResponse();
@@ -38,7 +36,8 @@ namespace MijnLijn.Data.Remote
             try
             {
                 var keyValues = new List<KeyValuePair<string, string>>();
-                foreach(int stopNumber in stopNumbers) {
+                foreach (int stopNumber in stopNumbers)
+                {
                     keyValues.Add(new KeyValuePair<string, string>("stopID", stopNumber.ToString()));
                 }
                 keyValues.Add(new KeyValuePair<string, string>("os", "android"));
@@ -61,46 +60,16 @@ namespace MijnLijn.Data.Remote
             return apiResponse;
         }
 
-        private static string FIELD_VALUE_SIGNATURE_PREFIX = "android_ML_";
-        private static string FIELD_VALUE_SIGNATURE_VERSION_NAME = "1.0";
-        private static string FIELD_KEY_STOP_ID = "stopID";
-        private static string FIELD_KEY_OS = "os";
-        private static string FIELD_VALUE_OS = "android";
-
-        private static string CreateSignature()
+        private void AddRequestToken(int[] stopNumbers)
         {
-            return FIELD_VALUE_SIGNATURE_PREFIX + FIELD_VALUE_SIGNATURE_VERSION_NAME;
+            string bodyString = CreateBodyString(stopNumbers);
+            string token = EncryptionUtil.CreateToken(bodyString);
+
+            client.DefaultRequestHeaders.Remove("x-token");
+            client.DefaultRequestHeaders.Add("x-token", token);
         }
 
-        private static string CreateToken(int[] stopNrs)
-        {
-            string secret = Constants.ApiSecret;
-            string signature = CreateSignature();
-            string bodyString = CreateBodyString(stopNrs);
-            string token = string.Format("{0}:{1}:{2}", secret, bodyString, signature);
-            Debug.WriteLine("MijnLijn D: token = " + token);
-            string hashedToken = CalculateSha1Hash(token).ToUpper();
-            Debug.WriteLine("MijnLijn D: hashed token = " + hashedToken);
-            return hashedToken;
-        }
-
-        private static string CalculateSha1Hash(string input)
-        {
-            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha1);
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hash = hasher.HashData(inputBytes);
-
-            //StringBuilder sb = new StringBuilder();
-            //for (int i = 0; i < hash.Length; i++)
-            //{
-            //    sb.Append(hash[i].ToString("X2"));
-            //}
-            //return sb.ToString();
-
-            return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
-        }
-
-        private static string CreateBodyString(int[] stopNrs)
+        private string CreateBodyString(int[] stopNrs)
         {
             string bodyString = "";
             foreach(int nr in stopNrs)
---
+++ MijnLijn/MijnLijn/Utils/EncryptionUtil.cs
@@ -0,0 +1,39 @@
+﻿using PCLCrypto;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MijnLijn.Utils
+{
+    public static class EncryptionUtil
+    {
+        private const string FIELD_VALUE_SIGNATURE_PREFIX = "android_ML_";
+        private const string FIELD_VALUE_SIGNATURE_VERSION_NAME = "1.0";
+      
+
+        public static string CreateSignature()
+        {
+            return FIELD_VALUE_SIGNATURE_PREFIX + FIELD_VALUE_SIGNATURE_VERSION_NAME;
+        }
+
+        public static string CreateToken(string bodyString)
+        {
+            string secret = Constants.ApiSecret;
+            string signature = CreateSignature();
+            string token = string.Format("{0}:{1}:{2}", secret, bodyString, signature);
+            Debug.WriteLine("MijnLijn D: token = " + token);
+            string hashedToken = CalculateSha1Hash(token).ToUpper();
+            Debug.WriteLine("MijnLijn D: hashed token = " + hashedToken);
+            return hashedToken;
+        }
+
+        private static string CalculateSha1Hash(string input)
+        {
+            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha1);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hash = hasher.HashData(inputBytes);
+
+            return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+        }
+    }
+}

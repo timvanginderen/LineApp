65f2f5ea361f6c947e426e9d01223dc3552b3c46
Tim Van Ginderen <timvanginderen@gmail.com>
2017-05-15 01:52:44 +0200

    Display stops nearby in listview

--- MijnLijn/MijnLijn/Views/NearbyTab.xaml.cs
+++ MijnLijn/MijnLijn/Views/NearbyTab.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MijnLijn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,25 @@ namespace MijnLijn
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NearbyTab : ContentPage
     {
+
+        private List<BusStop> nearbyStops;
+
         public NearbyTab()
         {
             InitializeComponent();
+
+            GetNearby();
+        }
+
+        private async void GetNearby()
+        {
+            nearbyStops = await App.Database.GetHaltesByDistanceAsync();
+            this.BindingContext = nearbyStops;
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
         }
     }
 }
